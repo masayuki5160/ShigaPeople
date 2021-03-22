8f8bad447be92718ebe326e004f8f8d5b26f4428
Masayuki Tanaka <masayuki5160@gmail.com>
2021-03-22 22:36:13 +0900

    Use const and remove unnecessary comments

--- Shiga/ShigaNewsViewModel.tsx
+++ Shiga/ShigaNewsViewModel.tsx
@@ -1,12 +1,12 @@
 import { parse } from 'fast-xml-parser';
 
 export const barTitle: String = '滋賀';
+const shigaRSSUrl = 'https://www.pref.shiga.lg.jp/file/rss/kensei_koho_e-shinbun_oshirase_index.rss';
 
 export const fetchRss = async () => {
-    let response = await fetch('https://www.pref.shiga.lg.jp/file/rss/kensei_koho_e-shinbun_oshirase_index.rss');
+    let response = await fetch(shigaRSSUrl);
     
     // TODO: 例外処理
-    console.log('result:', response.status);
     let responceText = await response.text();
     let obj = parse(responceText);
     let items = obj['rss']['channel']['item'];
