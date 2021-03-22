f474ab0664e9bd55f6ffa670ec8e4d03bec5c7e0
Masayuki Tanaka <masayuki5160@gmail.com>
2021-03-22 22:26:28 +0900

    Rename ShigaHomeScreen to ShigaStackScreen

--- TabNavigator.tsx
+++ TabNavigator.tsx
@@ -3,7 +3,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 // import Screens
 import AreaStackScreen from './AreaStackScreen';
-import ShigaHomeScreen from './Shiga/ShigaHomeScreen';
+import ShigaStackScreen from './Shiga/ShigaStackScreen';
 
 const Tab = createBottomTabNavigator();
 
@@ -11,7 +11,7 @@ export default function TabNavigator() {
   return (
       <NavigationContainer>
         <Tab.Navigator>
-          <Tab.Screen name="滋賀県" component={ShigaHomeScreen}/>
+          <Tab.Screen name="滋賀県" component={ShigaStackScreen}/>
           <Tab.Screen name="市町村" component={AreaStackScreen}/>
         </Tab.Navigator>
       </NavigationContainer>
