b0d44f3d213a6e2f9ac437a19a2d9d7d60a35d48
Masayuki Tanaka <masayuki5160@gmail.com>
2021-03-22 22:30:27 +0900

    Remove unused array

--- Shiga/ShigaNewsViewModel.tsx
+++ Shiga/ShigaNewsViewModel.tsx
@@ -2,17 +2,6 @@ import { parse } from 'fast-xml-parser';
 
 export const barTitle: String = '滋賀';
 
-export const news = [
-    {
-        text: "memomemo1",
-        createdAt: 1585574700001,
-    },
-    {
-        text: "memomemo2",
-        createdAt: 1585574700002,
-    },
-];
-
 export const fetchRss = async () => {
     let response = await fetch('https://www.pref.shiga.lg.jp/file/rss/kensei_koho_e-shinbun_oshirase_index.rss');
     
