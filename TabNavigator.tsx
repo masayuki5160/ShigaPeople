9dd3d84da4b9b1e048fbd145a68ced96552ada03
Masayuki Tanaka <masayuki5160@gmail.com>
2021-03-24 22:11:23 +0900

    Add icons to Tab bar

--- TabNavigator.tsx
+++ TabNavigator.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { AntDesign, Feather } from '@expo/vector-icons';
 // import Screens
 import AreaStackScreen from './AreaStackScreen';
 import ShigaStackScreen from './Shiga/ShigaStackScreen';
@@ -11,8 +12,26 @@ export default function TabNavigator() {
   return (
       <NavigationContainer>
         <Tab.Navigator>
-          <Tab.Screen name="滋賀県" component={ShigaStackScreen}/>
-          <Tab.Screen name="市町村" component={AreaStackScreen}/>
+          <Tab.Screen
+            name="滋賀県"
+            component={ShigaStackScreen}
+            options={{
+              //tabBarLabel: '',
+              tabBarIcon: ({ color, size }) => (
+                <AntDesign name="infocirlceo" size={24} color="black" />
+              ),
+            }}
+          />
+          <Tab.Screen 
+            name="市一覧"
+            component={AreaStackScreen}
+            options={{
+              //tabBarLabel: '',
+              tabBarIcon: ({ color, size }) => (
+                <Feather name="list" size={24} color="black" />
+              ),
+            }}
+          />
         </Tab.Navigator>
       </NavigationContainer>
   );
