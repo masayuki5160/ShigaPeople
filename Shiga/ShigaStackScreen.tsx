71c71c89678783f1213264d68da934709c9e05df
Masayuki Tanaka <masayuki5160@gmail.com>
2021-03-24 23:58:11 +0900

    Change header name

--- Shiga/ShigaStackScreen.tsx
+++ Shiga/ShigaStackScreen.tsx
@@ -6,12 +6,21 @@ import ShigaNewsViewModel from './ShigaNewsViewModel';
 import ShigaNewsWebViewScreen from './ShigaNewsWebViewScreen';
 
 const Stack = createStackNavigator();
+const headerTitleName = '滋賀県の情報';
 
 export default function ShigaStackScreen() {
     return(
         <Stack.Navigator initialRouteName='ShigaNewsViewModel'>
-            <Stack.Screen name='ShigaNewsViewModel' component={ShigaNewsViewModel}/>
-            <Stack.Screen name='ShigaNewsWebViewScreen' component={ShigaNewsWebViewScreen}/>
+            <Stack.Screen
+                name='ShigaNewsViewModel'
+                component={ShigaNewsViewModel}
+                options={{headerTitle: headerTitleName}}
+            />
+            <Stack.Screen
+                name='ShigaNewsWebViewScreen'
+                component={ShigaNewsWebViewScreen}
+                options={{headerTitle: headerTitleName}}
+            />
         </Stack.Navigator>
     );
 }
