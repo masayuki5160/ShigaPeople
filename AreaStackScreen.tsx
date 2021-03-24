40fb13ce5b4c621a4f3a7bc9d28bab7bc8e7642b
Masayuki Tanaka <masayuki5160@gmail.com>
2021-03-25 00:03:00 +0900

    Change header name

--- AreaStackScreen.tsx
+++ AreaStackScreen.tsx
@@ -11,10 +11,26 @@ const Stack = createStackNavigator();
 export default function areaStackScreen() {
     return(
       <Stack.Navigator initialRouteName='AreasListScreen'>
-        <Stack.Screen name='AreasListScreen' component={AreasListScreen}/>
-        <Stack.Screen name='ShigaNewsViewModel' component={ShigaNewsViewModel}/>
-        <Stack.Screen name='OtsuNewsViewModel' component={OtsuNewsViewModel}/>
-        <Stack.Screen name='ShigaNewsWebViewScreen' component={ShigaNewsWebViewScreen}/>
+        <Stack.Screen
+          name='AreasListScreen'
+          component={AreasListScreen}
+          options={{headerTitle: '各市の情報'}}
+        />
+        <Stack.Screen
+          name='ShigaNewsViewModel'
+          component={ShigaNewsViewModel}
+          options={{headerTitle: '滋賀県の情報'}}
+        />
+        <Stack.Screen
+          name='OtsuNewsViewModel'
+          component={OtsuNewsViewModel}
+          options={{headerTitle: '大津市の情報'}}
+        />
+        <Stack.Screen
+          name='ShigaNewsWebViewScreen'
+          component={ShigaNewsWebViewScreen}
+          options={{headerTitle: ''}}
+        />
       </Stack.Navigator>
     );
   }
--- Shiga/ShigaStackScreen.tsx
+++ Shiga/ShigaStackScreen.tsx
@@ -19,7 +19,7 @@ export default function ShigaStackScreen() {
             <Stack.Screen
                 name='ShigaNewsWebViewScreen'
                 component={ShigaNewsWebViewScreen}
-                options={{headerTitle: headerTitleName}}
+                options={{headerTitle: ''}}
             />
         </Stack.Navigator>
     );
