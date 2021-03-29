e3e527c88e4547e32681206a7395f39b211c03bd
Masayuki Tanaka <masayuki5160@gmail.com>
2021-03-30 00:38:18 +0900

    Implement #5

--- shiga/ShigaNewsViewModel.tsx
+++ shiga/ShigaNewsViewModel.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import NewsScreen from '../common/NewsScreen';
+import * as Notifications from 'expo-notifications';
+import Constants from 'expo-constants';
+import { Platform } from 'react-native';
 import ShigaNewsModel from './ShigaNewsModel';
 import {convertToViewData} from './ShigaNewsViewData';
 
@@ -11,6 +14,14 @@ interface State {
     news: {}
 }
 
+Notifications.setNotificationHandler({
+    handleNotification: async () => ({
+      shouldShowAlert: true,
+      shouldPlaySound: false,
+      shouldSetBadge: false,
+    }), 
+});
+
 export default class ShigaNewsViewModel extends React.Component<Props, State> {
     constructor(props: any) {
         super(props);
@@ -20,6 +31,12 @@ export default class ShigaNewsViewModel extends React.Component<Props, State> {
     }
 
     async componentDidMount() {
+        // TODO: fix later
+        const token = await this._registerForPushNotificationsAsync();        
+
+        Notifications.addNotificationReceivedListener(this._handleNotification);
+        Notifications.addNotificationResponseReceivedListener(this._handleNotificationResponse);
+
         const model = new ShigaNewsModel()
         const news = await model.fetchRss();
         const viewData = convertToViewData(news);
@@ -34,5 +51,45 @@ export default class ShigaNewsViewModel extends React.Component<Props, State> {
         return(
             <NewsScreen news={viewData}/>
         );
+    }
+
+    _handleNotification = notification => {
+        // this.setState({ notification: notification });
+        console.log('get push');
+    };
+      
+    _handleNotificationResponse = response => {
+        console.log('get push', response);
+    };
+
+    async _registerForPushNotificationsAsync() {
+        let token;
+        if (Constants.isDevice) {
+          const { status: existingStatus } = await Notifications.getPermissionsAsync();
+          let finalStatus = existingStatus;
+          if (existingStatus !== 'granted') {
+            const { status } = await Notifications.requestPermissionsAsync();
+            finalStatus = status;
+          }
+          if (finalStatus !== 'granted') {
+            alert('Failed to get push token for push notification!');
+            return;
+          }
+          token = (await Notifications.getExpoPushTokenAsync()).data;
+          console.log(token);
+        } else {
+          alert('Must use physical device for Push Notifications');
+        }
+      
+        if (Platform.OS === 'android') {
+          Notifications.setNotificationChannelAsync('default', {
+            name: 'default',
+            importance: Notifications.AndroidImportance.MAX,
+            vibrationPattern: [0, 250, 250, 250],
+            lightColor: '#FF231F7C',
+          });
+        }
+      
+        return token;  
     }
 }
